ab8a0b6fde34b1b6bbea05cf17d341fb6337217e
zy-yz <1090712762@qq.com>
2020-04-01 11:26:19 +0800

    验证

--- src/main/java/com/example/autocinfigure/ExampleService.java
+++ src/main/java/com/example/autocinfigure/ExampleService.java
@@ -1,5 +1,10 @@
 package com.example.autocinfigure;
 
+/**
+ *SpringBoot在启动的时候会去扫描项目中的jar包，寻找其中含有spring.factorios文件的jar包
+ *  根据这个文件来加载aotoconfig类
+ * 通过@conditional注解的条件，进行自动配置并加载到spring-context中
+ * */
 public class ExampleService {
 
     private String prefix;
