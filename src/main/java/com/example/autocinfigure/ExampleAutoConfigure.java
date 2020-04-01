5735e34dd3f768a6e33dbc53bb1eb0f8f45080be
zy-yz <1090712762@qq.com>
2020-04-01 11:15:39 +0800

    验证

--- src/main/java/com/example/autocinfigure/ExampleAutoConfigure.java
+++ src/main/java/com/example/autocinfigure/ExampleAutoConfigure.java
@@ -8,6 +8,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @ConditionalOnClass : 当classpath下发现该类的情况下自动配置
+ *
+ * */
 @Configuration
 @ConditionalOnClass(ExampleService.class)
 @EnableConfigurationProperties(ExampleServiceProperties.class)
@@ -16,6 +20,9 @@ public class ExampleAutoConfigure {
     @Autowired
     private ExampleServiceProperties properties;
 
+    /**
+     * @ConditionalOnMissingBean  当spring Context中不存在该Bean时
+     * */
     @Bean
     @ConditionalOnMissingBean
     @ConditionalOnProperty(prefix = "example.service",value = "enabled",havingValue = "true")
